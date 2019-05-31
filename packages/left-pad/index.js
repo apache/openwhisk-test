28f04d5e71acc5dc9e0e58954d536a78f7f77d4f
Priti Desai <pdesai@us.ibm.com>
2019-05-31 14:04:23 -0700

    adding left-pad action

---
+++ packages/left-pad/index.js
@@ -0,0 +1,7 @@
+function leftPadAction(args) {
+    const leftPad = require("left-pad")
+    const lines = args.lines || [];
+    return { padded: lines.map(l => leftPad(l, 30, ".")) }
+}
+
+exports.main = leftPadAction;
